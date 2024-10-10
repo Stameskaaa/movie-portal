399f5f0afa5908703ea8db17ac66c2f0fee2babd
Stameskaaa <chta02@mail.ru>
2024-10-11 00:29:04 +0500

    fix browse router

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import { store } from './store/store';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/movie-portal">
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
